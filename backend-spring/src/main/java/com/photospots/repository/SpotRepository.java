dadf2acd90fd487411e4bc36dc0b37cdb3322d44
Akbar <makbarali1106@gmail.com>
2025-12-20 16:16:58 -0500

    deleted backend-spring, and moved it into backend

--- backend-spring/src/main/java/com/photospots/repository/SpotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.photospots.repository;
-
-import com.photospots.model.Spot;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpotRepository extends JpaRepository<Spot, UUID> {
-    // TODO: add geospatial queries (nearby/search) using PostGIS functions
-}
